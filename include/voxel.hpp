5fd79cc65648739ee54343c2f9ea59c84da0c2ff
LegitYew0108 <legitlegit0108@gmail.com>
2025-10-10 13:14:48 +0900

    feat: add hash func

--- include/voxel.hpp
+++ include/voxel.hpp
@@ -1,9 +1,33 @@
 #pragma once
 
 #include <eigen3/Eigen/Core>
+#include <functional>
 #include <unordered_map>
+
 #include <vector>
+
 namespace PureNDT3D {
+struct VoxelIndex {
+  int x, y, z;
+
+  bool operator==(const VoxelIndex &other) const {
+    return x == other.x && y == other.y && z == other.z;
+  }
+};
+
+struct VoxelHash {
+  size_t hash_combine(size_t seed, size_t new_hash) const {
+    return new_hash + 0x9e3779b97f4a7c15LLU + (seed << 12) + (seed >> 4);
+  }
+
+  size_t operator()(const PureNDT3D::VoxelIndex &i) const {
+    size_t seed = std::hash<int>()(i.x);
+    seed ^= hash_combine(seed, std::hash<int>()(i.y));
+    seed ^= hash_combine(seed, std::hash<int>()(i.z));
+    return seed;
+  }
+};
+
 using Point3D = Eigen::Vector3d;
 using Covariance3D = Eigen::Matrix4d;
 
@@ -15,10 +39,12 @@ struct Voxel {
 
 class VoxelGrid {
 public:
+  VoxelGrid();
+
   void add_points(std::vector<Point3D>);
 
 protected:
-  std::unordered_map<Point3D, Voxel> voxels;
+  std::unordered_map<VoxelIndex, Voxel, VoxelHash> voxels_;
 };
 
 } // namespace PureNDT3D
